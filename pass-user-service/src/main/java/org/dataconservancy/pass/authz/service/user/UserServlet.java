81d3db1ea6e7a77c62362de0c7baa7c1319ab030
Aaron Birkland <apb@jhu.edu>
2018-05-20 17:53:41 -0400

    Increase logging and fix logic in user service

--- pass-user-service/src/main/java/org/dataconservancy/pass/authz/service/user/UserServlet.java
+++ pass-user-service/src/main/java/org/dataconservancy/pass/authz/service/user/UserServlet.java
@@ -19,6 +19,8 @@ package org.dataconservancy.pass.authz.service.user;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,11 +33,15 @@ import org.dataconservancy.pass.client.PassClient;
 import org.dataconservancy.pass.client.PassClientFactory;
 import org.dataconservancy.pass.model.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * This class gets an {@link AuthUser} object from the {@link ShibAuthUserProvider} and creates {@link User}
- * to be stored in the back end storage for PASS.
+ * This class gets an {@link AuthUser} object from the {@link ShibAuthUserProvider} and creates {@link User} to be
+ * stored in the back end storage for PASS.
  *
  * @author apb@jhu.edu
  * @author jrm@jhu.edu
@@ -43,14 +49,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SuppressWarnings("serial")
 public class UserServlet extends HttpServlet {
 
-    ObjectMapper mapper = new ObjectMapper();
+    Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
+    ExecutorService updater = Executors.newCachedThreadPool();
+
+    ObjectMapper mapper = new ObjectMapper().setSerializationInclusion(Include.NON_NULL);
+
     PassClient fedoraClient = PassClientFactory.getPassClient();
 
     AuthUserProvider provider = new ShibAuthUserProvider(fedoraClient);
 
     /**
-     * A method which calls  {@link ShibAuthUserProvider#getUser(HttpServletRequest)} to get an
-     * {@link AuthUser} in order to populate a {@link User} object and create/update and store it
+     * A method which calls {@link ShibAuthUserProvider#getUser(HttpServletRequest)} to get an {@link AuthUser} in
+     * order to populate a {@link User} object and create/update and store it
+     *
      * @param request - the {@code HttpServletRequest}
      * @param response - the {@code HttpServletResponse}
      * @throws IOException - if the
@@ -63,48 +75,52 @@ public class UserServlet extends HttpServlet {
 
         final AuthUser shibUser = provider.getUser(request);
         URI id = shibUser.getId();
-        final String email = shibUser.getEmail();
-        final String displayName = shibUser.getName();
-        final String institutionalId = shibUser.getInstitutionalId();
-        final String employeeid = shibUser.getEmployeeId();
 
-        User user = null;
+        final User user;
 
         // does the user already exist in the repository?
         if (id != null) {
+            LOG.info("User {} found at {}", shibUser.getPrincipal(), id);
             user = fedoraClient.readResource(id, User.class);
             boolean update = false;
 
-            //employeeId should never change
-            //each user provider will only adjust fields for which it is authoritative
-            //shib is authoritative for these
+            // employeeId should never change
+            // each user provider will only adjust fields for which it is authoritative
+            // shib is authoritative for these
 
-            if (user.getEmail() == null || !user.getEmail().equals(email)) {
-                user.setEmail(email);
+            if (user.getEmail() == null || !user.getEmail().equals(shibUser.getEmail())) {
+                user.setEmail(shibUser.getEmail());
                 update = true;
             }
-            if (user.getDisplayName() == null || !user.getDisplayName().equals(displayName)) {
-                user.setDisplayName(displayName);
+            if (user.getDisplayName() == null || !user.getDisplayName().equals(shibUser.getName())) {
+                user.setDisplayName(shibUser.getName());
                 update = true;
             }
-            if (user.getInstitutionalId() == null || !user.getInstitutionalId().equals(institutionalId)) {
-                user.setInstitutionalId(institutionalId);
+            if (user.getInstitutionalId() == null || !user.getInstitutionalId().equals(shibUser
+                    .getInstitutionalId())) {
+                user.setInstitutionalId(shibUser.getInstitutionalId());
                 update = true;
             }
 
             if (update) {
-                fedoraClient.updateResource(user);
+                LOG.info("User record for {} in repository is out of date, updating {} ", shibUser.getPrincipal(),
+                        user.getId());
+                updater.execute(() -> fedoraClient.updateResource(user));
             }
 
         } else {// no id, so we add new user to repository if eligible
             if (shibUser.isFaculty()) {
+                LOG.info("Creating new record for new user {}", shibUser.getPrincipal());
                 user = new User();
-                user.setLocalKey(employeeid);
-                user.setInstitutionalId(institutionalId);
-                user.setDisplayName(displayName);
-                user.setEmail(email);
+                user.setLocalKey(shibUser.getEmployeeId());
+                user.setInstitutionalId(shibUser.getInstitutionalId());
+                user.setDisplayName(shibUser.getName());
+                user.setEmail(shibUser.getEmail());
                 user.getRoles().add(User.Role.SUBMITTER);
                 id = fedoraClient.createResource(user);
+            } else {
+                LOG.warn("{} is not faculty, go away!", shibUser.getPrincipal());
+                user = null;
             }
         }
 
@@ -112,12 +128,13 @@ public class UserServlet extends HttpServlet {
         // and the up to date User object and valid id here
         // if the person is not eligible, id and user will be null
 
-        if (id != null) {
+        if (id != null && user != null) {
             try (Writer out = response.getWriter()) {
                 mapper.writerWithDefaultPrettyPrinter().writeValue(out, user);
                 response.setStatus(200);
             }
         } else {
+            LOG.info("{} not authorized", shibUser.getPrincipal());
             response.setStatus(401);
         }
     }
