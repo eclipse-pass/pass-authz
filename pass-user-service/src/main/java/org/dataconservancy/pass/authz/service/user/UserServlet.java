5871fb13cdb806cde8474d3e42e2b247b8b35325
Aaron Birkland <apb@jhu.edu>
2018-05-21 07:01:43 -0400

    Support x-forwarded-proto and host for user servce
    Additionallly flesh out UserServlet tests

--- pass-user-service/src/main/java/org/dataconservancy/pass/authz/service/user/UserServlet.java
+++ pass-user-service/src/main/java/org/dataconservancy/pass/authz/service/user/UserServlet.java
@@ -19,8 +19,7 @@ package org.dataconservancy.pass.authz.service.user;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URI;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.net.URISyntaxException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,6 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.dataconservancy.pass.authz.AuthUser;
 import org.dataconservancy.pass.authz.AuthUserProvider;
+import org.dataconservancy.pass.authz.LogUtil;
 import org.dataconservancy.pass.authz.ShibAuthUserProvider;
 import org.dataconservancy.pass.client.PassClient;
 import org.dataconservancy.pass.client.PassClientFactory;
@@ -51,13 +51,15 @@ public class UserServlet extends HttpServlet {
 
     Logger LOG = LoggerFactory.getLogger(UserServlet.class);
 
-    ExecutorService updater = Executors.newCachedThreadPool();
-
     ObjectMapper mapper = new ObjectMapper().setSerializationInclusion(Include.NON_NULL);
 
     PassClient fedoraClient = PassClientFactory.getPassClient();
 
     AuthUserProvider provider = new ShibAuthUserProvider(fedoraClient);
+
+    static {
+        LogUtil.adjustLogLevels();
+    }
 
     /**
      * A method which calls {@link ShibAuthUserProvider#getUser(HttpServletRequest)} to get an {@link AuthUser} in
@@ -81,7 +83,14 @@ public class UserServlet extends HttpServlet {
         // does the user already exist in the repository?
         if (id != null) {
             LOG.info("User {} found at {}", shibUser.getPrincipal(), id);
+
             user = fedoraClient.readResource(id, User.class);
+
+            if (user == null) {
+                LOG.warn("Resource {} does not exist, this should never happen", shibUser.getId());
+                response.setStatus(500);
+                return;
+            }
             boolean update = false;
 
             // employeeId should never change
@@ -105,7 +114,7 @@ public class UserServlet extends HttpServlet {
             if (update) {
                 LOG.info("User record for {} in repository is out of date, updating {} ", shibUser.getPrincipal(),
                         user.getId());
-                updater.execute(() -> fedoraClient.updateResource(user));
+                fedoraClient.updateResource(user);
             }
 
         } else {// no id, so we add new user to repository if eligible
@@ -118,6 +127,7 @@ public class UserServlet extends HttpServlet {
                 user.setEmail(shibUser.getEmail());
                 user.getRoles().add(User.Role.SUBMITTER);
                 id = fedoraClient.createResource(user);
+                user.setId(id);
             } else {
                 LOG.warn("{} is not faculty, go away!", shibUser.getPrincipal());
                 user = null;
@@ -129,6 +139,9 @@ public class UserServlet extends HttpServlet {
         // if the person is not eligible, id and user will be null
 
         if (id != null && user != null) {
+
+            rewriteUri(user, request);
+
             try (Writer out = response.getWriter()) {
                 mapper.writerWithDefaultPrettyPrinter().writeValue(out, user);
                 response.setStatus(200);
@@ -136,6 +149,95 @@ public class UserServlet extends HttpServlet {
         } else {
             LOG.info("{} not authorized", shibUser.getPrincipal());
             response.setStatus(401);
+        }
+    }
+
+    private void rewriteUri(User user, HttpServletRequest request) {
+
+        final Protocol proto = Protocol.of(request, user.getId());
+        final Host host = Host.of(request, user.getId());
+
+        final URI u = user.getId();
+
+        try {
+            user.setId(new URI(
+                    proto.get(),
+                    u.getUserInfo(),
+                    host.getHost(),
+                    host.getPort(),
+                    u.getPath(),
+                    u.getQuery(),
+                    u.getFragment()));
+        } catch (final URISyntaxException e) {
+            throw new RuntimeException("Error rewriting URI " + user.getId());
+        }
+
+    }
+
+    private static class Host {
+
+        final String host;
+
+        final int port;
+
+        static Host of(HttpServletRequest request, URI defaults) {
+            final String host = request.getHeader("host");
+            if (host != null && host != "") {
+                return new Host(host);
+            } else {
+                if (request.getRequestURL() != null) {
+                    return new Host(URI.create(request.getRequestURL().toString()).getHost());
+                } else {
+                    return new Host(defaults.getHost(), defaults.getPort());
+                }
+            }
+        }
+
+        private Host(String host, int port) {
+            this.host = host;
+            this.port = port;
+        }
+
+        private Host(String hostname) {
+            if (hostname.contains(":")) {
+                final String[] parts = hostname.split(":");
+                host = parts[0];
+                port = Integer.valueOf(parts[1]);
+            } else {
+                host = hostname;
+                port = -1;
+            }
+        }
+
+        String getHost() {
+            return host;
+        }
+
+        int getPort() {
+            return port;
+        }
+    }
+
+    private static class Protocol {
+
+        final String proto;
+
+        static Protocol of(HttpServletRequest request, URI defaults) {
+            if (request.getHeader("X-Forwarded-Proto") != null) {
+                return new Protocol(request.getHeader("X-Forwarded-Proto"));
+            } else if (request.getRequestURL() != null) {
+                return new Protocol(URI.create(request.getRequestURL().toString()).getScheme());
+            } else {
+                return new Protocol(defaults.getScheme());
+            }
+        }
+
+        private Protocol(String proto) {
+            this.proto = proto;
+        }
+
+        String get() {
+            return proto;
         }
     }
 
